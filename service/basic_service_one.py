07da50b88adf2107265691ffcae1b96f33d46aca
ramongduraes <ramongduraes@gmail.com>
2018-12-07 11:44:43 -0200

    bugging the service

--- service/basic_service_one.py
+++ service/basic_service_one.py
@@ -4,19 +4,16 @@ import sys
 import json
 import logging
 
+# Setting up a logger to log to a file.
+# This is extra relevant in the case of a process-type service because since SNET Daemon captures everything that is
+# printed to stdout
 log = logging.getLogger("basic_template")
 fh = logging.FileHandler('./service/executable_service.log')
 formatter = logging.Formatter("%(asctime)s - [%(levelname)8s] - %(name)s - %(message)s")
 fh.setFormatter(formatter)
 log.addHandler(fh)
-log.setLevel(logging.DEBUG)
-# formatter = logging.basicConfig(level=10, format="%(asctime)s - [%(levelname)8s] - %(name)s - %(message)s")
 
-"""
-Simple arithmetic service to test the Snet Daemon (gRPC), dApp and/or Snet-CLI.
-The user must provide the method (arithmetic operation) and
-two numeric inputs: "a" and "b".
-"""
+log.setLevel(logging.DEBUG)
 
 
 class AdditionServicer:
@@ -25,11 +22,9 @@ class AdditionServicer:
         self.b = 0
         self.result = 0
         # Just for debugging purpose.
+        print("Addition servicer created.")
         log.debug("AdditionServicer created")
 
-    # The method that will be exposed to the snet-cli call command.
-    # request: incoming data
-    # context: object that provides RPC-specific information (timeout, etc).
     def add(self, arguments):
         # In our case, request is a Numbers() object (from .proto file)
         self.a = arguments["a"]
@@ -45,12 +40,6 @@ class AdditionServicer:
 if __name__ == "__main__":
 
     log.debug("Running service.")
-
-    # Getting data from stdin
-    # for line in sys.stdin:
-    # while True:
-    #    line = sys.stdin.readline()
-    #    params = json.loads(line)
     method = sys.argv[1]
     log.debug("RECEIVED - Method: {}".format(method))
 
@@ -63,7 +52,7 @@ if __name__ == "__main__":
             for line in sys.stdin:
                 input_args += line
             log.debug("STDIN: {}".format(input_args))
-            params = json.loads(input_args)
+            params = json.loads(input_args)  # Converts from string to python dict
 
             add_class = AdditionServicer()
             result = add_class.add(params)
