e3bedc37fa2f24e27be5d6a7fabc0ab674c18660
Ramon Duraes <ramongduraes@gmail.com>
2019-01-29 22:50:23 -0200

    Changing service encoding to json

--- service/example-executable-service.py
+++ service/example-executable-service.py
@@ -23,17 +23,12 @@ if __name__ == "__main__":
     log.debug("RECEIVED - Method: {}".format(method))
 
     if method == "add":
-        # Read input parameters from stdin
-        with sys.stdin:
-            input_args = ""
-            for line in sys.stdin:
-                log.debug("RECEIVED - Stdin: {}". format(line))
-                input_args += line
-        params = json.loads(input_args)  # Converts from string to python dict
-        log.debug("STDIN: {}".format(input_args))
+        # Read input parameters from stdin and converts from string to python dict
+        parameters = json.loads(sys.argv[2])
+        log.debug("RECEIVED - Parameters: {}".format(parameters))
 
         # Add arguments
-        result = params["a"] + params["b"]  # Dictionary key names must match the specifications in .proto.
+        result = parameters["a"] + parameters["b"]  # Dictionary key names must match the specifications in .proto.
 
         # Build the resulting json from a dictionary
         return_dict = dict()
@@ -44,6 +39,7 @@ if __name__ == "__main__":
         sys.stdout.write(json_return)
         log.debug("STDOUT: {}".format(json_return))
         exit(0)
+
     else:
         # This condition will never happen because snet-cli won't allow methods unknown to it (i.e. not in .proto file).
         exit(1)
